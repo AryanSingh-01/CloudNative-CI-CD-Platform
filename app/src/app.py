4eec5b1152971289b15fcee8315c714763028b29
AryanSingh-01 <aryan1311002@gmail.com>
2026-01-09 08:21:18 +0530

    CI-CD Demo Prep

--- app/src/app.py
+++ app/src/app.py
@@ -1,19 +1,22 @@
 from flask import Flask, jsonify
 
 app = Flask(__name__)
+APP_VERSION = "v1.0.0"
 
 @app.route("/")
 def home():
-    return jsonify({
-        "message": "CloudNative CI/CD Platform",
-        "status": "running"
-    })
+    return f"""
+    <h1>CloudNative CI/CD Platform</h1>
+    <p>Status: Deployed successfully via automated CI/CD pipeline</p>
+    <p>App Version: {APP_VERSION}</p>
+    """
 
 @app.route("/health")
 def health():
-    return jsonify({
-        "status": "healthy"
-    })
+    return jsonify(
+        status="UP",
+        service="cloudnative-ci-cd-platform"
+    )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
